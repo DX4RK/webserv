5e92fa9e3432f10725a056d5451f028cf345d2d8
nolan <noldiane@c1r8p3.42nice.fr>
2025-06-02 22:59:00 +0200

    added a include file with all the libraries needed

---
+++ includes/_libs.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+#include <poll.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/wait.h>
