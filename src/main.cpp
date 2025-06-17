e9df2fe3da172d09fc187b6bd635421b24b1a352
nolan <firesocietygames@gmail.com>
2025-06-17 18:46:05 +0200

    Removed: removed poll leaks/errors by handling signals

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,42 @@
 #include "bindingSocket.hpp"
 #include "listenSocket.hpp"
 
+volatile sig_atomic_t g_keepRunning = 1;
+
+void handle_sigint(int signum) {
+	(void)signum;
+	g_keepRunning = 0;
+}
+
 int main(void) {
 
+	signal(SIGINT, handle_sigint);
+	signal(SIGTERM, handle_sigint);
+
 	Config *config = new Config("./config/default.conf");
-	
+
 	std::vector<int> ports = config->getServerPorts();
 	std::vector<BindingSocket*> bindingSockets;
-	
+
 	for (size_t i = 0; i < ports.size(); i++) {
 		BindingSocket* socket = new BindingSocket(AF_INET, SOCK_STREAM, 0, INADDR_ANY, config, ports[i]);
 		bindingSockets.push_back(socket);
 	}
-	
-	ListenSocket listener(bindingSockets, config);
-	listener.launch();
+
+	try {
+		ListenSocket listener(bindingSockets, config);
+		listener.launch(g_keepRunning);
+	} catch (const std::exception &e) {
+		std::cerr << "Fatal error: " << e.what() << std::endl;
+	}
 
 	for (size_t i = 0; i < bindingSockets.size(); i++) {
 		delete bindingSockets[i];
 	}
+
 	delete config;
+
+
+
 	return 0;
 }
